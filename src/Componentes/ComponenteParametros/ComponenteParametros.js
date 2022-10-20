ceaf729cf8966ae0b08b02029929c665394e99f7
Viviana218 <104119912+Viviana218@users.noreply.github.com>
2022-10-20 16:49:16 -0500

    inicializando

---
+++ src/Componentes/ComponenteParametros/ComponenteParametros.js
@@ -0,0 +1,26 @@
+import { useParams } from "react-router-dom";
+
+const ComponenteParametros = props => {
+
+    const {palabra, color, bg} = useParams();
+
+    return(
+        <div>
+            {
+                isNaN(palabra) ?
+                    <div style={
+                        //si existe la variable color
+                        color?
+                            {color:color, backgroundColor:bg}
+                        :
+                            null
+                    }
+                    >La palabra es: {palabra}</div>
+                :
+                    <div>El número es:{palabra}</div>
+            }
+        </div>
+    )
+}
+
+export default ComponenteParametros;
